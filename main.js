73ffa9e1e9e268d7ef1fe22ef3299c2aeae7bacb
IDEIDEIDE <75457931+IDEIDEIDE@users.noreply.github.com>
2021-05-02 18:51:07 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,16 +13,30 @@ recognition.onresult = function (event){
 
     document.getElementById("input").innerHTML = content;
     console.log(content)
-    speak();
+
+    if(content=="take my selfie"){
+        speak()
+    }
+
+
 }
 function speak(){
     var synth = window.speechSynthesis;
-    speech_data = document.getElementById("input").value;
+    speech_data = "taking your selfie in 5 seconds";
     var Utterthis = new SpeechSynthesisUtterance(speech_data);
     synth.speak(Utterthis)
     Webcam.attach(camera)
+
+    setTimeout(function(){
+        takeSnap();
+        save();
+    },5000)
 }
 
+function takeSnap(){
+    Webcam.snap(function(anyname){
+        document.getElementById("results").innerHTML = "<img id = 'coolimage' src = " + anyname + ">"
+    })}
 Webcam.set({
     width: 350,
     height: 350,
@@ -31,3 +45,9 @@ Webcam.set({
 })
 camera = document.getElementById("camera")
 
+function save(){
+    link = document.getElementById("link")
+    image = document.getElementById("coolimage").src
+    link.href = image
+    link.click()
+}
